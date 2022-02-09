d2ab2e9e48920ef18218ef47c2538de27259d19d
rui <ruixue.fi@gmail.com>
2022-02-09 20:00:47 +0200

    #11: first trial to persist Company entity

---
+++ security-search-engine/src/main/java/rx/rest/CompanyResource.java
@@ -0,0 +1,29 @@
+package rx.rest;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.json.JsonObject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+import rx.jpaentity.Company;
+import rx.jpaentity.Region;
+@Path("/company")
+@ApplicationScoped
+public class CompanyResource {
+	@PersistenceContext
+	EntityManager entityManager;
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void create(JsonObject companyJson) {
+		Company company = new Company();
+		company.setCompanyCode("VZ");
+		company.setHeadquarter(Region.AMERICA);
+		company.setName("Verizon");
+		entityManager.persist(company);
+	}
+
+}
