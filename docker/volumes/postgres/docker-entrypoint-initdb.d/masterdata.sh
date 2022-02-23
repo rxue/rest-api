66b5d120c20d4a75de7f237808d46c3408cb9068
rui <ruixue.fi@gmail.com>
2022-02-23 23:08:14 +0200

    #12 create multiple databases with Shell implemented
     enough to create the database without relevant user atm

--- docker/volumes/postgres/docker-entrypoint-initdb.d/masterdata.sh
+++ docker/volumes/postgres/docker-entrypoint-initdb.d/masterdata.sh
@@ -1,6 +1,11 @@
-echo "  Creating user and database 'masterdata'"
-psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
-    CREATE USER masterdata;
-    CREATE DATABASE masterdata;
-    GRANT ALL PRIVILEGES ON DATABASE masterdata TO masterdata;
+create_single_db () {
+  local _db_name=${1}
+  echo "Creating database ${_db_name}"
+  psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
+    CREATE DATABASE ${_db_name};
 EOSQL
+}
+db_names=("masterdata" "security")
+for db_name in ${db_names[@]}; do
+  create_single_db ${db_name}
+done
