07086098c896e85da2d2f3c8360f3515d48016ff
Rui Xue <rui.xue@fasolutions.com>
2025-03-17 23:24:38 +0200

    run_all_tests script improved with more description

--- account/tests/run_all_tests.sh
+++ account/tests/run_all_tests.sh
@@ -1,7 +1,9 @@
 source ../generate_keypair.sh ../src/main/resources/httpscert.p12
+echo "execute unit tests and integration tests"
 mvn --file ../pom.xml clean verify
 mvn --file ../pom.xml spring-boot:run &
 sleep 8
 npm install
+echo "execute end-to-end tests on the API"
 npm test
 ps aux |grep spring-boot |awk '{print $2}' |xargs kill
